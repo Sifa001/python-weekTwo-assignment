eab881d65b5b9b8b30e9b9cc558c7d6c6be0a985
Sifa001 <115398391+Sifa001@users.noreply.github.com>
2025-03-20 10:15:52 +0300

    Completed Assignment

--- list.py
+++ list.py
@@ -1,1 +1,26 @@
+# creating a list
+my_list = []
 
+#appending elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#inserting a value to the list
+my_list.insert(1,15)
+
+#extending the list
+my_list.extend([50,60,70])
+
+#removing an item from the list
+my_list.pop()
+
+#sorting the list in ascending order
+my_list.sort()
+
+#finding and printing the index of 30
+indexOf30 = my_list.index(30)
+print(indexOf30)
+
+print(my_list)
